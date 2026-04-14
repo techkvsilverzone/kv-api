bc6de1d57becf8fca7a0049f9fd7c7cf54c1e559
Vinoth Sasikumar <vinoth@cascen.com>
2026-04-14 17:51:19 +0530

    feat: Update MongoDB connection to use fallback URI if config is not set

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -3,7 +3,8 @@ import { config } from '../config';
 import Logger from './logger';
 
 export const connectMongo = async (): Promise<void> => {
-  await mongoose.connect(config.mongoUri);
+  const uri = config.mongoUri || 'mongodb://localhost:27017/kv-silver-zone';
+  await mongoose.connect(uri);
   Logger.info('Connected to MongoDB');
 };
 
